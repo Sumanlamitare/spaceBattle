db2c74412e520c1f770134ee3735f2ce593a4dee
Suman Adhikari <sumanadhikari@Sumans-MacBook-Air.local>
2025-01-05 20:28:12 -0500

    work on game logic, implement all the necessary functions

--- app.js
+++ app.js
@@ -12,6 +12,11 @@
 //The battle function will take two class ship class as parameter performs accordingly
 // the retreat function will end the game
 
+//btns selector
+const startBtn = document.getElementById("start");
+const attackBtn = document.getElementById("attack");
+const retreatBtn = document.getElementById("retreat");
+
 //function to log the results in the div
 
 function logResults(result) {
@@ -23,6 +28,8 @@ function logResults(result) {
   messageElement.textContent = result;
 
   resultDiv.appendChild(messageElement);
+
+  resultDiv.scrollTop = resultDiv.scrollHeight;
 }
 
 let captain;
@@ -48,14 +55,13 @@ class Uss extends Ship {
     while (alienship.hull > 0 && this.hull > 0) {
       if (Math.random() < this.accuracy) {
         alienship.hull -= this.firepower;
-        addToConsol(`${alienship.name} has taken ${this.firepower} damage!`);
+        logResults(`${alienship.name} has taken ${this.firepower} damage!`);
         if (alienship.hull <= 0) {
           //check if Alienship is dead
           logResults(`${alienship.name} has been destroyed!!`);
-
           break;
         }
-      } else addToConsol(`${this.name} missed!!`);
+      } else logResults(`${this.name} missed!!`);
       if (Math.random() < alienship.accuracy) {
         this.hull -= alienship.firepower;
         logResults(`${this.name} has taken ${alienship.firepower} damage!`);
@@ -69,16 +75,134 @@ class Uss extends Ship {
 }
 
 class Aliens extends Ship {
-  constructor(name) {
+  constructor(name, backgroundImage) {
     super(name);
     this.hull = Math.floor(Math.random() * 4) + 3;
     this.firepower = Math.floor(Math.random() * 3) + 2;
     this.accuracy = Math.round((Math.random() * (0.8 - 0.6) + 0.6) * 10) / 10;
+    this.backgroundImage = backgroundImage;
+  }
+}
+class Game {
+  constructor() {
+    this.playerShip = new Uss("Aliens worst Nightmare");
+    this.aliens = [
+      new Aliens(
+        "Warlord",
+        "url('https://easy-peasy.ai/cdn-cgi/image/quality=80,format=auto,width=700/https://fdczvxmwwjwpwbeeqcth.supabase.co/storage/v1/object/public/images/bb6d78bb-1cd9-427b-8373-00e8f9191743/c5c4f225-4b8a-4a1e-92a5-3b8babe476d0.png')"
+      ),
+      new Aliens(
+        "Kaido Army",
+        "url('https://img.freepik.com/premium-photo/close-up-black-white-alien-head-black-background_958108-99401.jpg')"
+      ),
+      new Aliens(
+        "Doflamingo",
+        "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYTx4I1WtjrDLzCqdcQ2WIJGB-p00GDOz2pw&s')"
+      ),
+      new Aliens(
+        "Blackbeard",
+        "url('https://img.freepik.com/premium-photo/alien-black-background_238683-1607.jpg')"
+      ),
+      new Aliens(
+        "Madara",
+        "url('https://img.freepik.com/premium-photo/mysterious-alien-being-with-large-eyes-holds-glowing-orb-its-hands_1022970-56222.jpg?w=360')"
+      ),
+    ];
+    this.defeatedAliens = 0;
+    this.currentAlien = null;
+  }
+
+  //start game
+
+  startGame() {
+    logResults("Game Started");
+    this.selectRandomAlien();
+  }
+
+  //function to select random aliens
+  selectRandomAlien() {
+    if (this.aliens.length === 0) {
+      logResults(
+        `Congratulation captain ${captain}. You have defeated all alien ships`
+      );
+      return;
+    }
+    const randomIndex = Math.floor(Math.random() * this.aliens.length);
+    this.currentAlien = this.aliens[randomIndex];
+    this.updateBackground(this.currentAlien.backgroundImage);
+    this.battle();
+  }
+
+  //update background function
+  updateBackground(backgroundImage) {
+    const alienDiv = document.querySelector(".alienShip");
+    alienDiv.style.backgroundImage = backgroundImage;
+
+    alienDiv.style.backgroundSize = "cover";
+    alienDiv.style.backgroundPosition = "center";
+  }
+
+  //battle function
+  battle() {
+    //initiate the attack between player ship and alien ship
+    this.playerShip.attack(this.currentAlien);
+
+    if (this.currentAlien.hull <= 0) {
+      //alien ship is destroyed
+      this.aliens = this.aliens.filter((alien) => alien !== this.currentAlien);
+      this.defeatedAliens++;
+
+      this.showActionBtns();
+    } else if (this.playerShip.hull <= 0) {
+      logResults("Game over! You have been destroyed!");
+      this.gameOver();
+    }
+  }
+  showActionBtns() {
+    attackBtn.style.display = "inline-block";
+    retreatBtn.style.display = "inline-block";
+
+    attackBtn.onclick = () => {
+      attackBtn.style.display = "none";
+      retreatBtn.style.display = "none";
+      this.selectRandomAlien();
+    };
+
+    retreatBtn.onclick = () => {
+      attackBtn.style.display = "none";
+      retreatBtn.style.display = "none";
+      this.retreat();
+    };
+  }
+
+  retreat() {
+    logResults(
+      `You retreated. You defeated ${this.defeatedAliens} alien(s). The game is over.`
+    );
+  }
+
+  gameOver() {
+    attackBtn.style.display = "none";
+    retreatBtn.style.display = "none";
+
+    logResults(
+      `Game over, your ship is destroyed. You defeated ${this.defeatedAliens}`
+    );
   }
 }
 
-let ship = new Uss("suman");
-// console.log(ship);
-let alien = new Aliens("Domgadon");
-// console.log(alien);
-ship.attack(alien);
+//function to get captains name
+
+function whenLoaded() {
+  captain = prompt(
+    "Welcome captain, please enter your name to continue to the game."
+  );
+}
+window.onload = function () {
+  whenLoaded();
+};
+const game = new Game();
+
+document.querySelector("#start").addEventListener("click", function () {
+  game.startGame();
+});
